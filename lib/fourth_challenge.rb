a6d14b32ed4bc5ec0e6c4239b449bef25a8d3938
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-02-02 02:13:25 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -13,7 +13,9 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        
+      :Benvolio => {:name => "Benvolio",:age => "17"
+      :attitude => "worried"}, :Mercutio => {:name => "Mercutio",:age => "18"
+      :attitude => "hot-headed"}
       ]
    }
 }
