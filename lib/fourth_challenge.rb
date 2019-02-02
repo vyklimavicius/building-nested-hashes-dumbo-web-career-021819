a6fc3958a5c79d306305ac06ec9ad79f437e6746
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-02-02 02:24:14 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -18,5 +18,7 @@ def fourth_challenge
 
 # epic_tragedy[:montague][:hero][:status] = "dead"
 # epic_tragedy[:capulet][:heroine][:status] = "dead"
-
 end
+
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
